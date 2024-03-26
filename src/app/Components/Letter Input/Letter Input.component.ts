7fd1d7a6d21b6a8d8dfb6cc6c5cf9078497f3563
AnatoliSmorin <AnatoliSmorin@outlook.com>
2024-03-26 10:18:27 +0100

    keyup listener created

--- src/app/Components/Letter Input/Letter Input.component.ts
+++ src/app/Components/Letter Input/Letter Input.component.ts
@@ -1,14 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-letter-input',
     standalone: true,
     imports: [
         CommonModule,
+        FormsModule
     ],
     templateUrl: './Letter Input.component.html',
     styleUrl: './Letter Input.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LetterInputComponent { }
+export class LetterInputComponent 
+{
+    letter:string = "";
+    onChange():void
+    {
+        this.letter.toUpperCase();
+        console.log(this.letter);        
+    }
+ }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LetterInputComponent } from './Components/Letter Input/Letter Input.component';
+import { Directive } from '@angular/core';
+
+@Directive()
+class UserInput {
+}
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, LetterInputComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  host: {
+    '(document: keyup.backspace)': 'onBackspace()',
+    '(document: keyup.delete)': 'onBackspace()',
+    '(document: keyup.enter)': 'onEnter()',
+    '(document: keyup)': 'onKeyUp($event.key)',
+  }
 })
 export class AppComponent {
+
+  guess:string = "";
+
+  @HostListener('keyup', ['$event.key'])
+  onKeyUp(input:string):void
+  {
+    const re = new RegExp(/[a-z,A-Z]/);
+    if(re.test(input) && input.length == 1)
+    {
+      this.guess += input.toUpperCase();
+      console.log(input + " - " + this.guess);
+    }
+  }
+  onBackspace():void
+  {
+    if(this.guess.length > 0)
+    {
+      this.guess = this.guess.substring(0, this.guess.length - 1);
+      console.log("Backspace - " + this.guess);
+    }
+  }
+  onEnter():void
+  {
+    console.log("Enter")
+  }
   title = 'Wordle';
 }
