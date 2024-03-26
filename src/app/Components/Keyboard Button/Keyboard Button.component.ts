a469cca8697d1d613f6d94c06b56e6d1051b0e72
AnatoliSmorin <AnatoliSmorin@outlook.com>
2024-03-26 22:03:47 +0100

    self-centering logo in header

--- src/app/Components/Keyboard Button/Keyboard Button.component.ts
+++ src/app/Components/Keyboard Button/Keyboard Button.component.ts
@@ -1,18 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { KeyboardMatrixComponent } from '../Keyboard Matrix/Keyboard Matrix.component';
 
 @Component({
     selector: 'app-keyboard-button',
     standalone: true,
     imports: [
-        CommonModule,
+        CommonModule,KeyboardMatrixComponent
     ],
     templateUrl: './Keyboard Button.component.html',
     styleUrl: './Keyboard Button.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class KeyboardButtonComponent {
-    @Input() label:string|undefined;
+    @Input()label:string = '*';
+    dummy:string = '*';
     onClick():void
     {
         console.log("click - " + this.label);
